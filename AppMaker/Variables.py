d111baab8abbf43fe9e2a4c2aeb72dca2871237e
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2024-11-07 20:40:03 +0100

    Create Variables.py

---
+++ AppMaker/Variables.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+BASE_DICT = {
+    "Window": {},
+    "Buttons": {},
+    "Labels": {},
+    "Entry": {}
+}
+
+BASE_BTN = {
+    "text": "Button",
+     "bg": "blue",
+     "fg": "white",
+     "fontSize": 12,
+     "state": NORMAL,
+     "relief": RAISED,
+     "width": 8,
+     "border": 1,
+     "pady": 0,
+     "padx": 0,
+     "side": NONE,
+     "fill": NONE,
+}
+
+BASE_LABEL = {
+    "text": "Button",
+     "bg": "blue",
+     "fg": "white",
+     "fontSize": 12,
+     "wraplength": 24,
+     "underline": -1,
+     "relief": RAISED,
+     "border": 1,
+     "pady": 0,
+     "padx": 0,
+     "side": NONE,
+     "fill": NONE,
+}
